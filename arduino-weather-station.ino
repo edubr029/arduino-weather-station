a40c3ecde5ba7884145e9655b8c89d218542c6e9
Eduardo Fonseca <me@eduu.xyz>
2023-12-16 20:57:56 -0300

    W.I.P. - Add new features and improve menu functionality

--- arduino-weather-station.ino
+++ arduino-weather-station.ino
@@ -4,72 +4,136 @@
 #include "dht.h"
 #include "rtc.h"
 #include "ldr.h"
+#include "bmp.h"
+#include "btn.h"
 
-void setup() {
-  startLCD();
-  startRTC();
+void showHumidity();
+void showTemperature();
+void showTime();
 
-  dht.begin();
+namespace arrow{
+    void right(){
+        lcd_d.setCursor(0,1);
+        lcd_d.write(byte(0));
+    }
+
+    void left(){
+        lcd_d.setCursor(15,1);
+        lcd_d.write(byte(1));
+    }
+
+    void both(){
+        right();
+        left();
+    }
+
+    void blink(int side){
+      // 0 - left arrow | 1 - right arrow
+        switch(side){
+            case 0:
+                left();
+                delay(500);
+                lcd_d.setCursor(0,1);
+                lcd::blank(1);
+                delay(500);
+            break;
+            case 1:
+                right();
+                delay(500);
+                lcd_d.setCursor(15,1);
+                lcd::blank(1);
+                delay(500);
+            break;
+        }
+    }
 }
 
-void loop() {
-  mainMenu();
-  checkDHT();
-  ldr_test();
-}
+namespace menu{
+  void main(){
+    if(button::left == HIGH) arrow::blink(0);
+    else if(button::right == HIGH) arrow::blink(1);
+    else arrow::both();
 
-void mainMenu(){
-  showHumidity();
-  showTemperature();
-  showArrows();
-  showTime();
-  show_lux();
-}
+    if(button::select == HIGH) lcd_d.clear();
+    else {
+      showHumidity();
+      showTemperature();
+      showTime();
+    }
+  }
 
-void showArrows(){
-  printRightArrow();
-  printLeftArrow();
-}
+  void pressure(){
+    lcd_d.setCursor(1,0);
+    lcd_d.print(bmp::pressure());
+    lcd_d.print("Pa");
+  }
 
-void printRightArrow(){
-  lcd.setCursor(0,1);
-  lcd.write(byte(0));
-}
+  void lux(){
+    lcd_d.setCursor(1,1);
+    lcd_d.print("Lux");
+    lcd_d.print(ldr::lux());
+    lcd_d.print("lx");
+  }
 
-void printLeftArrow(){
-  lcd.setCursor(15,1);
-  lcd.write(byte(1));
-}
-
-void show_lux(){
-  lcd.setCursor(0,0);
-  lcd.print(ldr::lux());
-}
-
-void showHumidity(){
-  int humidity = dht.readHumidity();
-
-  lcd.setCursor(2,1);
-  lcd.write(byte(2));
-  lcd.print(humidity);
-  lcd.print("%");
-  lcd.print("   ");
-}
-
-void showTemperature(){
-  int temperature = dht.readTemperature();
-
-  lcd.setCursor(9,1);
-  lcd.write(byte(3));
-  lcd.print(temperature);
-  lcd.write(byte(223));
-  lcd.print("C");
-  lcd.print(" ");
+  void HI(){
+    lcd_d.setCursor(1,0);
+    lcd_d.print(dht::heat_index());
+    lcd_d.write(byte(223)); // Degree Symbol
+    lcd_d.print("C");
+  }
 }
 
 void showTime(){
-  lcd.setCursor(10,0);
-  clockHM();
+  lcd_d.setCursor(10,0);
+  rtc::clock();
 
   delay(1000);
+}
+
+void showHumidity(){
+  int hum = dht::humidity();
+
+  lcd_d.setCursor(2,1);
+  lcd_d.write(byte(2));   // Water Drop
+  lcd_d.print(hum);
+  lcd_d.print("%");
+  lcd::blank(3);
+}
+
+void showTemperature(){
+  int temp = dht::temperature();
+  
+  lcd_d.setCursor(9,1);
+  lcd_d.write(byte(3));   // Thermometer
+  lcd_d.print(temp);
+  // lcd_d.print((int)bmp::temperature());
+  lcd_d.write(byte(223)); // Degree Symbol
+  lcd_d.print("C"); 
+  lcd::blank(1);
+}
+
+// Arduino Default Functions
+void setup() {
+  lcd::start();
+  dht::start();
+  // bmp::start();
+  rtc::start(0); // 0 - Set the RTC with the current date and time
+                 // 1 - Set the RTC with a specific date and time
+
+  button::start();
+}
+
+void loop() {
+  menu::main();
+  
+  // Test Functions
+  // ldr_test();
+
+  // Debug Functions
+  button::debug();
+
+  // Serial Test Functions
+  // serialBMP();
+  // serialDHT();
+  // serialRTC();
 }
