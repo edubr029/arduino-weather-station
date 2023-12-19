df8f639d1b03a3f3785e5d4062bdfcfe5e82ebf7
Eduardo Fonseca <me@eduu.xyz>
2023-12-19 20:36:25 -0300

    Add button debounce and refactor button code

--- btn.h
+++ btn.h
@@ -2,31 +2,47 @@
 #define select_button 9
 #define right_button 8
 
+const int buttons[3] = {left_button, select_button, right_button};
+
 namespace button{
+    unsigned long interval = 200;
+    unsigned long buttonPressedTime = 0;
+    bool buttonState = false;
+
     void start(){
-        pinMode(left_button, INPUT_PULLUP);
-        pinMode(right_button, INPUT_PULLUP);
-        pinMode(select_button, INPUT_PULLUP);
+        for(int i = 0; i < 3; i++)
+            pinMode(buttons[i], INPUT_PULLUP);
     }
 
-    bool left(){
-        return digitalRead(right_button);
+    bool read(int buttonPin){
+        unsigned long currentMillis = millis();
+        int reading = digitalRead(buttonPin);
+
+        if(reading == LOW && !buttonState){
+            // Button was just pressed
+            buttonState = true;
+            buttonPressedTime = currentMillis;
+        }
+
+        if(reading == HIGH && buttonState){
+            // Button was just released
+            buttonState = false;
+            interval = 200;  // Reset the interval to 200ms
+        }
+        
+        return buttonState;
     }
 
-    bool right(){
-        return digitalRead(right_button);
-    }
-
-    bool select(){
-        return digitalRead(select_button);
-    }
-
+    bool left() {return read(left_button);}
+    bool right() {return read(right_button);}
+    bool select() {return read(select_button);}
+    
     void debug(){
         Serial.print("Left: ");
         Serial.print(left());
-        Serial.print(" | Right: ");
-        Serial.print(right());
         Serial.print(" | Select: ");
-        Serial.println(select());
+        Serial.print(select());
+        Serial.print(" | Right: ");
+        Serial.println(right());
     }
 }
